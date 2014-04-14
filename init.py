3a1595679b048cebc4daf16d155a92da671c9950
Denis <mashkov@gmail.com>
2014-04-14 10:27:22 -0700

    Update init.py

--- init.py
+++ init.py
@@ -10,13 +10,13 @@ db = db.connect(host="myhost", user="user", passwd="psswd", db="base", charset='
 c = db.cursor()
 c.execute(""" SELECT  s.* FROM `push`.`push_subscribers` s where device_type = 'apns'   and last_time > unix_timestamp() - 90*24*60*60
   and disable = 'N'  
-  or   device_id = 'c988ddfac4568955381d9090f42f45833115431de10b76835d79a4b219161e86' 
+  or   device_id = '32hex device id ' 
   order by last_time """)
 
 result = c.fetchall()
 
 session = Session()
-con = session.get_connection("push_production", cert_file="gazeta_production_full.pem")
+con = session.get_connection("push_production", cert_file="production_full.pem")
 kwargs = { "aps" : {'sound':'default' , 'badge':1 , 'alert': 'test' , 'type':0 , 'id':'6074161'}}
 
 for row in result: 
